713f22c89df54050eb2a1be0ce68464c89e52769
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-19 15:58:33 -0500

    installed bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import bootstrap stylesheet
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 createApp(App).mount('#app')
