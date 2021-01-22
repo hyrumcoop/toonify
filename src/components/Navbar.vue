db4b5b3468721cf62e7e6f842feb1e65133c643a
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-22 10:23:59 -0500

    added Github link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav class='navbar navbar-dark bg-dark'>
     <div class='container-fluid'>
       <span class='navbar-brand mb-0 h1'>Toonify</span>
+      <a href='https://github.com/hyrumcoop/toonify'><i class="bi bi-github text-white fs-4"></i></a>
     </div>
   </nav>
 </template>
