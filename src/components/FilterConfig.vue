e7d3e2ddf853dbd37de1d88877c34907ba65530d
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-21 23:59:16 -0500

    created visual aspects of FilterConfig sidebar component

---
+++ src/components/FilterConfig.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class='sidebar d-flex flex-column shadow-sm'>
+    <h5 class='sidebar-heading'>FILTER CONFIG</h5>
+
+    <div class='config-group'>
+      <label for='edges-input' class='form-label fw-bold'>EDGES</label>
+      <input
+        type='range'
+        class='form-range'
+        min='0'
+        max='5'
+        id='edges-input'
+        :disabled='running'
+        :value='modelValue.edges'
+        @input='updateConfig($event, "edges")'
+      >
+    </div>
+
+    <div class='config-group'>
+      <label for='blur-input' class='form-label fw-bold'>BLUR</label>
+      <input
+        type='range'
+        class='form-range'
+        min='0'
+        max='5'
+        id='blur-input'
+        :disabled='running'
+        :value='modelValue.blur'
+        @input='updateConfig($event, "blur")'
+      >
+    </div>
+
+    <div class='config-group'>
+      <label for='quantization-input' class='form-label fw-bold'>QUANTIZATION</label>
+      <input
+        type='range'
+        class='form-range'
+        min='0'
+        max='5'
+        id='quantization-input'
+        :disabled='running'
+        :value='modelValue.quantization'
+        @input='updateConfig($event, "quantization")'
+      >
+    </div>
+
+    <div class='config-group mb-0'>
+      <button
+        class='btn w-100 fw-bold'
+        :class='{"btn-primary": !running, "btn-secondary": running}'
+      >{{ running ? 'STOP' : 'FILTER' }}</button>
+    </div>
+
+    <div v-if='!running'>
+      <hr>
+
+      <div class='config-group'>
+        <button class='btn btn-primary w-100 fw-bold'>UPLOAD IMAGE</button>
+      </div>
+
+      <div class='config-group'>
+        <button class='btn btn-secondary w-100 fw-bold'>LOAD URL</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterConfig',
+  props: {
+    running: Boolean,
+    modelValue: {
+      type: Object,
+      default: () => ({
+        edges: 3,
+        blur: 3,
+        quantization: 3
+      })
+    }
+  },
+  methods: {
+    updateConfig(e, prop) {
+      this.$emit('update:modelValue', {
+        ...this.modelValue,
+        [prop]: e.target.value
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.sidebar {
+  width: 200px;
+  height: 100%;
+
+  border-left: 1px solid #ccc;
+}
+
+.sidebar-heading {
+  margin-top: 8px;
+  padding: 0px 8px;
+}
+
+.config-group {
+  margin-bottom: 8px;
+  padding: 0px 8px;
+
+  font-size: 14px;
+}
+
+.config-group label {
+  margin: 0px;
+}
+
+.config-group button {
+  font-size: inherit;
+}
+
+</style>
