c5e9dd1bc04469c0aa6a319ae91534c494d25c91
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-21 22:06:01 -0500

    added save button to ImageViewer

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue'
 // import bootstrap stylesheet
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).mount('#app')
